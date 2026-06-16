5f6d4197be9abfe40969a72ae010b4d5b047c96d
Sankalp Dubey <156274057+0Sankalp@users.noreply.github.com>
2026-06-16 20:54:42 +0530

    Revision

---
+++ 0level_problem/main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[]args){
+    
+        Scanner input = new Scanner(System.in);
+        //System.out.println(input.next());  hey how are you 
+    //                                       hey
+System.out.println(input.nextLine());
+//                                      hey how are you 
+//                                      hey how are you
+
+        //        System.out.print("Hello World");
+    }
+    
+}

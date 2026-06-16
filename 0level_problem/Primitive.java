c2eacef410f691e120037145f57ac9f0badff758
Sankalp Dubey <156274057+0Sankalp@users.noreply.github.com>
2026-06-16 20:55:39 +0530

    Revision

---
+++ 0level_problem/Primitive.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Primitive {
+    public static void main(String[]args){
+        //int rollno = 64;
+        //char letter = 'r';
+        //float marks = 98.67f;
+        //double largeDecimalnumber = 7554458.2445656468;
+        //boolean check = true;
+      /*Scanner input = new Scanner(System.in);
+        System.out.println("Please Enter  a number:");
+        int rollno = input.nextInt();
+        System.out.println("Your rollno is:"+ rollno);
+        
+        Please Enter  a number:143
+        Your rollno is:143
+
+         */
+        Scanner input = new Scanner(System.in);
+      //  String name = input.next();
+       // System.out.println(name);
+        /*
+Sankalp Dubey 
+Sankalp */
+      // int num1 = input.nextInt();
+       //int num2 = input.nextInt();
+       float  num1 = input.nextFloat();
+       float  num2 = input.nextFloat();
+
+       float  Sum=num1+num2;
+       System.out.print("Sum:"+Sum);
+
+       /*
+       85.5
+545.5
+Sum:631.0
+       */
+
+
+    }
+}

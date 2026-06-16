aa9512161d3eabd8db2fc4d9fb8e952e7064df08
Sankalp Dubey <156274057+0Sankalp@users.noreply.github.com>
2026-06-16 20:55:10 +0530

    Revision

---
+++ 0level_problem/Typecasting.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Typecasting {
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+       // float num = input.nextFloat();
+      //  System.out.print(num);// 85
+                             //  85.0
+  
+    // Automatic Typecasting promotion in  Expression
+
+   // int a = 257;
+    //byte b= (byte) (a);// 257 % 256 = 1
+   // System.out.print(b);
+
+   /*  byte a = 40;
+    byte b = 50;
+    byte c = 100;
+    int d = (a*b)/c;
+    //System.out.print(d);
+    //20 */
+    
+
+
+  /*Basic Loop */
+  //int a=10;
+
+  //if(a==10){
+   // System.out.print("hello world");
+ // }//hello world
+  
+ /*int count = 1;
+ while(count!=5 ){
+    System.out.println(count);
+    count++;
+ }
+  
+1
+2
+3
+4
+*/
+
+for(int count =1;count!=5;count++){
+    System.out.println(count);
+}
+
+    }
+}
+
+
